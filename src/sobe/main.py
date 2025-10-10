0f572c0d05c2832e32ff485b2ff49b36f953965e
Liz Balbuena <ekevoo@gmail.com>
2025-10-10 19:39:23 -0400

    strongly type config

--- src/sobe/main.py
+++ src/sobe/main.py
@@ -6,50 +6,14 @@ import mimetypes
 import pathlib
 import sys
 import time
-import tomllib
 import warnings
 
 import boto3
 import botocore.exceptions
-import platformdirs
 import urllib3.exceptions
 
+from .config import CONFIG
 
-def load_config():
-    path = platformdirs.PlatformDirs("sobe", "balbuena.ca").user_config_path / "config.toml"
-    if path.exists():
-        with path.open("rb") as f:
-            payload = tomllib.load(f)
-            if payload.get("aws", {}).get("bucket", "example-bucket") != "example-bucket":
-                return payload
-
-    defaults = """
-        # sobe configuration
-        url = "https://example.com/"
-
-        [aws]
-        bucket = "example-bucket"
-        cloudfront = "E1111111111111"
-
-        [aws.session]
-        # If you already have AWS CLI set up, don't fill keys here.
-        # region_name = "..."
-        # profile_name = "..."
-        # aws_access_key_id = "..."
-        # aws_secret_access_key = "..."
-
-        [aws.client]
-        verify = true
-    """
-    defaults = "\n".join(line.strip() for line in defaults.lstrip().splitlines())
-    path.parent.mkdir(parents=True, exist_ok=True)
-    path.write_text(defaults)
-    print("Created config file at the path below. You must edit it before use.")
-    print(path)
-    sys.exit(1)
-
-
-CONFIG = load_config()
 write = functools.partial(print, flush=True, end="")
 print = functools.partial(print, flush=True)  # type: ignore
 warnings.filterwarnings("ignore", category=urllib3.exceptions.InsecureRequestWarning)
@@ -57,9 +21,9 @@ warnings.filterwarnings("ignore", category=urllib3.exceptions.InsecureRequestWar
 
 def main() -> None:
     args = parse_args()
-    session = boto3.Session(**CONFIG["aws"]["session"])
-    s3 = session.resource("s3", **CONFIG["aws"]["client"])
-    bucket = s3.Bucket(CONFIG["aws"]["bucket"])  # type: ignore
+    session = boto3.Session(**CONFIG.aws.session)
+    s3 = session.resource("s3", **CONFIG.aws.service)
+    bucket = s3.Bucket(CONFIG.aws.bucket)  # type: ignore
     for path, key in zip(args.paths, args.keys):
         if args.delete:
             delete(bucket, key)
@@ -70,7 +34,7 @@ def main() -> None:
 
 
 def upload(bucket, path: pathlib.Path, remote_path: str) -> None:
-    write(f"{CONFIG['url']}{remote_path} ...")
+    write(f"{CONFIG.url}{remote_path} ...")
     type_guess, _ = mimetypes.guess_type(path)
     extra_args = {"ContentType": type_guess or "application/octet-stream"}
     bucket.upload_file(str(path), remote_path, ExtraArgs=extra_args)
@@ -78,7 +42,7 @@ def upload(bucket, path: pathlib.Path, remote_path: str) -> None:
 
 
 def delete(bucket, remote_path: str) -> None:
-    write(f"{CONFIG['url']}{remote_path} ...")
+    write(f"{CONFIG.url}{remote_path} ...")
     obj = bucket.Object(remote_path)
     try:
         obj.load()
@@ -93,16 +57,16 @@ def delete(bucket, remote_path: str) -> None:
 def invalidate(session: boto3.Session) -> None:
     write("Clearing cache ...")
     ref = datetime.datetime.now().astimezone().isoformat()
-    cloudfront = session.client("cloudfront", **CONFIG["aws"]["client"])
+    cloudfront = session.client("cloudfront", **CONFIG.aws.service)
     batch = {"Paths": {"Quantity": 1, "Items": ["/*"]}, "CallerReference": ref}
-    invalidation = cloudfront.create_invalidation(DistributionId=CONFIG["aws"]["cloudfront"], InvalidationBatch=batch)
+    invalidation = cloudfront.create_invalidation(DistributionId=CONFIG.aws.cloudfront, InvalidationBatch=batch)
     write("ok.")
     invalidation_id = invalidation["Invalidation"]["Id"]
     status = ""
     while status != "Completed":
         time.sleep(3)
         write(".")
-        response = cloudfront.get_invalidation(DistributionId=CONFIG["aws"]["cloudfront"], Id=invalidation_id)
+        response = cloudfront.get_invalidation(DistributionId=CONFIG.aws.cloudfront, Id=invalidation_id)
         status = response["Invalidation"]["Status"]
     print("complete.")
 
@@ -112,7 +76,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("-y", "--year", type=int, default=datetime.date.today().year, help="change year directory")
     parser.add_argument("-i", "--invalidate", action="store_true", help="invalidate CloudFront cache")
     parser.add_argument("-d", "--delete", action="store_true", help="delete instead of upload")
-    parser.add_argument("--policy", action="store_true", help="display IAM policy requirements and exit")
+    parser.add_argument("--policy", action="store_true", help="generate IAM policy requirements and exit")
     parser.add_argument("files", nargs="*", help="Source files.")
     args = parser.parse_args()
 
@@ -138,18 +102,20 @@ def parse_args() -> argparse.Namespace:
 
 
 def dump_policy() -> None:
-    session = boto3.Session(**CONFIG["aws"]["session"])
-    sts = session.client("sts", **CONFIG["aws"]["client"])
-    caller = sts.get_caller_identity()["Arn"]
-    account_id = caller.split(":")[4]
+    try:
+        session = boto3.Session(**CONFIG.aws.session)
+        sts = session.client("sts", **CONFIG.aws.service)
+        account_id = sts.get_caller_identity()["Account"]
+    except botocore.exceptions.ClientError:
+        account_id = "YOUR_ACCOUNT_ID"
     actions = """
         s3:PutObject s3:GetObject s3:ListBucket s3:DeleteObject
         cloudfront:CreateInvalidation cloudfront:GetInvalidation
     """.split()
     resources = [
-        f"arn:aws:s3:::{CONFIG['aws']['bucket']}",
-        f"arn:aws:s3:::{CONFIG['aws']['bucket']}/*",
-        f"arn:aws:cloudfront::{account_id}:distribution/{CONFIG['aws']['cloudfront']}",
+        f"arn:aws:s3:::{CONFIG.aws.bucket}",
+        f"arn:aws:s3:::{CONFIG.aws.bucket}/*",
+        f"arn:aws:cloudfront::{account_id}:distribution/{CONFIG.aws.cloudfront}",
     ]
     statement = {"Effect": "Allow", "Action": actions, "Resource": resources}
     policy = {"Version": "2012-10-17", "Statement": [statement]}
